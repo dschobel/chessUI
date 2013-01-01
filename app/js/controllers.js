7db603e276124225e75b59227aa6976a46c7ff77
Daniel Schobel <dschobel@gmail.com>
2013-01-01 22:34:58 +1100

    implemented reset and fast-forward to end

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,7 +3,7 @@
 function ChessCtrl($scope) {
 
     $scope.chess = new Chess();
-    $scope.history = []
+    $scope.local_history = []
     $scope.chess = new Chess();
     $scope.reset = function(){
         if($scope.pgn){
@@ -11,23 +11,36 @@ function ChessCtrl($scope) {
             $scope.chess.load_pgn($scope.pgn);
             $scope.boardText = '\n' + $scope.chess.ascii();
         }
-        $scope.history = [];
+        $scope.local_history = [];
     }
     $scope.load = function(){
         $scope.pgn = $scope.newPGN
         $scope.status = $scope.chess.load_pgn($scope.pgn);
         $scope.boardText = '\n' +$scope.chess.ascii();
     }
+    $scope.end = function(){
+        $scope.chess.load_pgn($scope.pgn);
+        $scope.local_history = [];
+        $scope.boardText = '\n' +$scope.chess.ascii();
+    }
+    $scope.restart = function(){
+        if($scope.chess.history()){
+            $scope.chess.load_pgn($scope.pgn);
+            $scope.local_history = $scope.chess.history().reverse();
+            $scope.chess.reset();
+            $scope.boardText = '\n' +$scope.chess.ascii();
+        }
+    }
     $scope.undo = function(){
         if($scope.chess.history()){
-            $scope.history.push($scope.chess.history()[$scope.chess.history().length-1])
+            $scope.local_history.push($scope.chess.history()[$scope.chess.history().length-1])
             $scope.chess.undo();
             $scope.boardText = '\n' +$scope.chess.ascii();
         }
     }
     $scope.advance = function(){
-        if($scope.history){
-            $scope.chess.move($scope.history.pop())
+        if($scope.local_history){
+            $scope.chess.move($scope.local_history.pop())
             $scope.boardText = '\n' +$scope.chess.ascii();
         }
     }
