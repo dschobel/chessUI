a95e155df072c19102826f720e1f656c61cd2e85
Daniel Schobel <dschobel@gmail.com>
2013-01-01 21:33:42 +1100

    dropped reset button, switched to column layout

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -9,26 +9,26 @@ function ChessCtrl($scope) {
         if($scope.pgn){
             $scope.status = "resetted";
             $scope.chess.load_pgn($scope.pgn);
-            $scope.boardText = $scope.chess.ascii();
+            $scope.boardText = '\n' + $scope.chess.ascii();
         }
         $scope.history = [];
     }
     $scope.load = function(){
         $scope.pgn = $scope.newPGN
         $scope.status = $scope.chess.load_pgn($scope.pgn);
-        $scope.boardText = $scope.chess.ascii();
+        $scope.boardText = '\n' +$scope.chess.ascii();
     }
     $scope.undo = function(){
         if($scope.chess.history()){
             $scope.history.push($scope.chess.history()[$scope.chess.history().length-1])
             $scope.chess.undo();
-            $scope.boardText = $scope.chess.ascii();
+            $scope.boardText = '\n' +$scope.chess.ascii();
         }
     }
     $scope.advance = function(){
         if($scope.history){
             $scope.chess.move($scope.history.pop())
-            $scope.boardText = $scope.chess.ascii();
+            $scope.boardText = '\n' +$scope.chess.ascii();
         }
     }
 }
